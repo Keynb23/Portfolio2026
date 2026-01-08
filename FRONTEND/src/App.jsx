38c63c596416862d1098008bf4e458df9d3253a0
Key'n Brosdahl <keynb1999@gmail.com>
2026-01-08 15:04:29 -0600

    GOT MILK?

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -5,16 +5,30 @@ import Experience from "./components/Experience/Experience";
 import Skills from "./components/Skills/Skills";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import Sammy from "./assets/Sammy.png";
+
+const SammyBG = () => {
+  return (
+    <div className="Sammy_IMG">
+      <img src={Sammy} alt="Sammy" />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Hero />
+
       <FeaturedWork />
       <Experience />
       <Skills />
       <Footer />
+      <div className="Sammy_container">
+        <SammyBG />
+        <h1 className="MILKY">GOT MILK?</h1>
+      </div>
     </div>
   );
 }
